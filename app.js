4a381b7dd906b171df9f7bf22677e683eb435874
harigustave <harigustave@gmail.com>
2023-12-22 08:02:53 -0800

    New changes

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require('express');
+
+// Crreate an express app
+const app = express();
+
+// Register view engine npm installed for templates to create/render dynamic web contents instead of static web only
+app.set('view engine', 'ejs');
+
+//listen for request
+app.listen(3000);
+
+// Respond to requests
+app.get('/', (req, res)=>{
+    const blogs=[
+        
+    ];
+    res.render('home', {title:'Home', blogs:blogs});
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about', {title:'About Us'});
+});
+
+app.get('/blogs/create', (req,res)=>{
+    res.render('create', {title:'Create New Blog'});
+});
+
+//Set the 404 page
+app.use((req,res)=>{
+    res.status(404).render('404', {title:'404'});
+});
