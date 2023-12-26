abfe0cc881a4c80e28c8a75d6ed427385919ef9e
harigustave <harigustave@gmail.com>
2023-12-26 02:41:27 -0800

    added static files middleware for css refactoring

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const morgan = require('morgan');
 
 // Crreate an express app
 const app = express();
@@ -8,6 +9,10 @@ app.set('view engine', 'ejs');
 
 //listen for request
 app.listen(3000);
+
+//middleware for static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 // Respond to requests
 app.get('/', (req, res)=>{
